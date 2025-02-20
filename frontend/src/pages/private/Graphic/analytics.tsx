c462554196d313492d1620ab90305eab1eb9ce41
Erik Soza <esoza20@alumnos.utalca.cl>
2025-02-20 16:49:27 -0300

    Ajuste en componente tabla y flexibilidad en el uso de los componentes

--- frontend/src/pages/private/Graphic/analytics.tsx
+++ frontend/src/pages/private/Graphic/analytics.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Grafico from "../../../components/Grafico.tsx";
 import AppBar from "../../../components/common/AppBar.tsx";
+import Tabla from "../../../components/common/Tabla.tsx"
 import { Box, Typography } from "@mui/material";
 import { useLocation } from "react-router-dom";
 
@@ -22,6 +23,19 @@ return (
               <h1>{title}</h1>
             </Typography>
             <Grafico type={chartType} devUI={devUI} threshold={threshold}/>
+        </Box>
+        <Box
+            sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                flexDirection: "column", 
+            }}
+        >
+            <Typography gutterBottom sx={{ color: 'text.secondary', fontSize: 14 }}>
+              <h1>Tabla de datos</h1>
+            </Typography>
+            <Tabla devUI={devUI}/>
         </Box>
     </AppBar>
 );
